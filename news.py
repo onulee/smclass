9556ee41250ab20736bbe6eb14c00295c426861c
onulee <65047894+onulee@users.noreply.github.com>
2024-10-25 17:52:52 +0900

    Update news.py

--- news.py
+++ news.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+# email 발송관련
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 url = 'https://news.naver.com/main/ranking/popularDay.naver'
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
@@ -24,6 +29,41 @@ for i,r_list in enumerate(r_lists):
   
 f.close()  
 
+# 메일보내기
+smtpName = "smtp.naver.com"
+smtpPort = 587
+
+# 자신의 네이버메일주소,pw, 받는사람이메일주소
+sendEmail = "onulee@naver.com"
+pw = "LDN53XPJ58R9"
+recvEmail = "onulee@naver.com"
+
+title = "랭킹뉴스"
+content = "랭킹뉴스 파일을 첨부합니다."
+
+msg = MIMEMultipart()
+msg["Subject"] = title
+msg["From"] = sendEmail
+msg["To"] = recvEmail
+msg.attach(MIMEText(content))
+
+# 파일첨부
+with open("c1025/news.txt",'rb') as f:
+  attachment = MIMEApplication(f.read()) # 파일첨부
+  attachment.add_header('Content-Disposition','attachment',filename="news.txt")
+  msg.attach(attachment)
+
+s = smtplib.SMTP(smtpName,smtpPort)
+s.starttls() # 보안인증
+# 2단계 보안설정이 되어 있는 경우는 에러 발생
+# 인증키 발급을 받아야 함.
+s.login(sendEmail,pw)
+s.sendmail(sendEmail,recvEmail,msg.as_string()) 
+print("msg : ")
+print(msg.as_string())
+s.quit()
+
+print("메일이 발송되었습니다.!")
 
 
 
