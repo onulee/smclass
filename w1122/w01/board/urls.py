b88ae75b409f178bba1ce9984b14555557289ff7
sm <onulee@naver.com>
2024-11-22 18:19:39 +0900

    계층형게시판 답변달기

--- w1122/w01/board/urls.py
+++ w1122/w01/board/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
+    path('breply/<int:bno>/',views.breply,name='breply' ),  # 답변달기
+    path('bdelete/<int:bno>/',views.bdelete,name='bdelete' ),  # 글삭제
     path('bmodify/<int:bno>/',views.bmodify,name='bmodify' ),  # 글수정
     path('bview/<int:bno>/',views.bview,name='bview' ),  # 글상세보기
     path('bwrite/',views.bwrite,name='bwrite' ),         # 글쓰기
--- w1122/w01/board/views.py
+++ w1122/w01/board/views.py
@@ -3,6 +3,43 @@ from board.models import Board
 from django.db.models import Max
 from django.contrib import messages
 from django.db.models import F
+import datetime
+
+# 답변달기
+def breply(request,bno):
+  if request.method == "GET":
+    print("게시글 번호 : ",bno)
+    qs = Board.objects.get(bno=bno)
+    context = {"board":qs}
+    return render(request,'breply.html',context)
+  else:
+    bgroup = int(request.POST.get("bgroup"))  # str타입 -> int타입변경
+    bstep = int(request.POST.get("bstep"))
+    bindent = int(request.POST.get("bindent"))
+    id = request.POST.get("id")
+    btitle = request.POST.get("btitle")
+    bcontent = request.POST.get("bcontent")
+    print("bgroup 번호 : ",bgroup)
+    
+    ## 다른 답변달기에 bstep을 1씩 증가시켜줌.
+    qs = Board.objects.filter(bgroup = bgroup,bstep__gt=bstep)
+    qs.update(bstep = F('bstep')+1)
+    
+    # bgroup : 부모의 bgroup 입력
+    Board.objects.create(id=id,btitle=btitle,bcontent=bcontent,bgroup=bgroup\
+      ,bstep=bstep+1,bindent=bindent+1)
+    
+    # return redirect('board:blist')
+    return render(request,'blist.html',{"r_msg":"1"})
+    
+   
+
+
+# 글삭제
+def bdelete(request,bno):
+  print("게시글 번호 : ",bno)
+  Board.objects.get(bno=bno).delete()
+  return render(request,'blist.html',{"d_msg":bno})
 
 # 글수정페이지, 글수정저장
 def bmodify(request,bno):
@@ -34,10 +71,30 @@ def bview(request,bno):
   
   # 조회수 1증가 - filter : update()
   qs = Board.objects.filter(bno=bno)
-  qs.update(bhit=F('bhit') + 1)
   context = {'board':qs[0]}
+  response = render(request,'bview.html',context)
+  
+  # 시간설정
+  tomorrow = datetime.datetime.replace(datetime.datetime.now(), hour=23, minute=59, second=0)
+  expires = datetime.datetime.strftime(tomorrow, "%a, %d-%b-%Y %H:%M:%S GMT")
+  
+  if request.COOKIES.get('cookie_bhit') is not None:
+        cookies = request.COOKIES.get('cookie_bhit')
+        print("cookies : ",cookies)
+        cookies_list = cookies.split('|')
+        print(cookies_list)
+        if str(bno) not in cookies_list:
+            response.set_cookie('cookie_bhit', cookies + f'|{bno}', expires =expires)
+            qs.update(bhit=F('bhit') + 1)
+            return response
+  # [4] hit를 check하는 쿠키가 없는 경우
+  else:
+      response.set_cookie('cookie_bhit', bno, expires =expires)
+      qs.update(bhit=F('bhit') + 1)
+      return response
+
+  return response
     
-  return render(request,'bview.html',context)
 
 
 
