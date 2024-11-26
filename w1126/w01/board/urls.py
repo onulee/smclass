75c9a2eeba3c87c5b48274529b0ea539895ccce1
sm <onulee@naver.com>
2024-11-26 17:39:03 +0900

    하단넘버링 계층형게시판

--- w1126/w01/board/urls.py
+++ w1126/w01/board/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('bview/<int:bno>/', views.bview, name="bview"),
     path('bdelete/<int:bno>/', views.bdelete, name="bdelete"), # 글삭제
     path('bupdate/<int:bno>/', views.bupdate, name="bupdate"), # 글수정
+    path('breply/<int:bno>/', views.breply, name="breply"), # 답글쓰기
 ]
--- w1126/w01/board/views.py
+++ w1126/w01/board/views.py
@@ -4,12 +4,69 @@ from member.models import Member
 from datetime import datetime
 from django.db.models import Q
 from django.db.models import F
+from django.core.paginator import Paginator
 
-# 글수정
+
+# 게시판리스트
+def blist(request):
+  npage = int(request.GET.get('npage',1)) #넘어온 현재페이지
+  qs = Board.objects.all().order_by("-bgroup","bstep")
+  # 하단페이지처리(넘버링)
+  paginator = Paginator(qs,10)
+  blist = paginator.get_page(npage)  # 1페이지 10개
+  context = {"blist":blist, 'npage':npage}
+  return render(request,'blist.html',context)
+
+
+
+# 답글쓰기페이지, 답글쓰기저장
+def breply(request,bno):
+  if request.method == "GET":
+    qs = Board.objects.get(bno=bno)
+    context = {"board":qs}
+    return render(request,'breply.html',context)
+  else:
+    bno = request.POST.get("bno")
+    id = request.session.get("session_id")
+    member = Member.objects.get(id=id)
+    bgroup = int(request.POST.get("bgroup"))
+    bstep = int(request.POST.get("bstep"))
+    bindent = int(request.POST.get("bindent"))
+    qs = Board.objects.filter(bgroup=bgroup,bstep__gt=bstep)
+    qs.update(bstep = F('bstep')+1)
+    
+    btitle = request.POST.get("btitle")
+    bcontent = request.POST.get("bcontent")
+    bfile = request.FILES.get("bfile","")
+    Board.objects.create(member=member,btitle=btitle,bcontent=bcontent,bfile=bfile\
+      , bgroup=bgroup,bstep=bstep+1,bindent=bindent+1 )
+    
+    context = {'rmsg':bno}
+    return render(request,'breply.html',context)
+
+
+# 글수정페이지, 글수정저장
 def bupdate(request,bno):
-  qs = Board.objects.get(bno=bno)
-  context = {"board":qs}
-  return render(request,'bupdate.html',context)
+  if request.method == "GET":
+    qs = Board.objects.get(bno=bno)
+    context = {"board":qs}
+    return render(request,'bupdate.html',context)
+  else:
+    bno = request.POST.get("bno")
+    btitle = request.POST.get("btitle")
+    bcontent = request.POST.get("bcontent")
+    bfile = request.FILES.get("bfile","")
+    
+    # 글수정저장
+    qs = Board.objects.get(bno=bno)
+    qs.btitle = btitle
+    qs.bcontent = bcontent
+    if bfile:
+      qs.bfile = bfile
+    qs.save()
+    
+    context = {'umsg':bno}
+    return render(request,'bupdate.html',context)
 
 
 
@@ -21,6 +78,7 @@ def bdelete(request,bno):
 
 # 글상세보기
 def bview(request,bno):
+  npage = request.GET.get("npage",1)
   qs = Board.objects.get(bno=bno)
   
   ## 이전글, 다음글
@@ -32,7 +90,7 @@ def bview(request,bno):
   day1 = datetime.replace(datetime.now(),hour=23,minute=59,second=59)
   expires = datetime.strftime(day1,"%a, %d-%b-%Y %H:%M:S GMT")
   print("날짜 : ",expires)
-  context = {"board":qs,"prev_board":prev_qs,"next_board":next_qs}
+  context = {"board":qs,"prev_board":prev_qs,"next_board":next_qs,"npage":npage}
   response = render(request,'bview.html',context)
   ## 쿠키확인
   if request.COOKIES.get("cookie_boardNo") is not None:
@@ -76,8 +134,4 @@ def bwrite(request):
     return render(request,'bwrite.html',context)
 
 
-# 게시판리스트
-def blist(request):
-  qs = Board.objects.all().order_by("-bgroup","bstep")
-  context = {"blist":qs}
-  return render(request,'blist.html',context)
+
